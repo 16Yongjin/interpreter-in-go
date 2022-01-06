bc8592040a390a1741ae2302675db2cc1301c476
16Yongjin <yongjin0802@gmail.com>
2022-01-07 01:23:10 +0900

    add string concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -179,6 +179,8 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case operator == "==":
 		return nativeBoolToBooleanObject(left == right)
 	case operator == "!=":
@@ -206,6 +208,21 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBooleanObject(leftVal < rightVal)
 	case ">":
 		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
+	default:
+		return newError("%s: %s %s %s", UNKNOWN_OPERATOR_ERROR, left.Type(), operator, right.Type())
+	}
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	switch operator {
+	case "+":
+		return &object.String{Value: leftVal + rightVal}
 	case "==":
 		return nativeBoolToBooleanObject(leftVal == rightVal)
 	case "!=":
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -171,10 +171,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			fmt.Sprintf("%s: %s", IDENTIFIER_NOT_FOUND_ERROR, "foobar"),
 		},
-		// {
-		// 	`"Hello" - "World"`,
-		// 	fmt.Sprintf("%s: %s - %s", UNKNOWN_OPERATOR_ERROR, object.STRING_OBJ, object.STRING_OBJ),
-		// },
+		{
+			`"Hello" - "World"`,
+			fmt.Sprintf("%s: %s - %s", UNKNOWN_OPERATOR_ERROR, object.STRING_OBJ, object.STRING_OBJ),
+		},
 		// {
 		// 	`{"name": "Monky"}[fn(x) { x }];`,
 		// 	fmt.Sprintf("unusable as hash key: %s", object.FUNCTION_OBJ),
@@ -264,6 +264,20 @@ func TestStringLiteral(t *testing.T) {
 
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
 
